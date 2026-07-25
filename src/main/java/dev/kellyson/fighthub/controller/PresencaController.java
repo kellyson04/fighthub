6954a6442665f7f06fee6f5f056a7d28ecb052d3
Kell <gilsonvigilante@outlook.com>
2026-07-25 20:39:36 -0300

    feat: adiciona endpoint para registrar presença

---
+++ src/main/java/dev/kellyson/fighthub/controller/PresencaController.java
@@ -0,0 +1,24 @@
+package dev.kellyson.fighthub.controller;
+
+import dev.kellyson.fighthub.service.PresencaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/presencas")
+@RequiredArgsConstructor
+public class PresencaController {
+
+    private final PresencaService presencaService;
+
+    @PostMapping("{matriculaId}")
+    public ResponseEntity<Void> registrarPresenca(@PathVariable Long matriculaId) {
+        presencaService.confirmarPresenca(matriculaId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
