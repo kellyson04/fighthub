28cf6ae0b36e3c54fd82eab84d7a1d587502d9f0
Kell <gilsonvigilante@outlook.com>
2026-07-25 00:51:43 -0300

    feat: adiciona endpoint para cadastrar turmas

---
+++ src/main/java/dev/kellyson/fighthub/controller/TurmaController.java
@@ -0,0 +1,26 @@
+package dev.kellyson.fighthub.controller;
+
+import dev.kellyson.fighthub.dto.TurmaRequest;
+import dev.kellyson.fighthub.service.TurmaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/turmas")
+@RequiredArgsConstructor
+public class TurmaController {
+
+    private final TurmaService turmaService;
+
+    @PostMapping
+    public ResponseEntity<Void> cadastrarTurma(@RequestBody @Valid TurmaRequest turmaRequest) {
+        turmaService.cadastrarTurma(turmaRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
