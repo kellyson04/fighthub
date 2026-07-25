f4200c65d22650ceda26d30ada7a77f0035d5f66
Kell <gilsonvigilante@outlook.com>
2026-07-24 23:02:19 -0300

    feat: adiciona endpoint para definir perfil de treino

--- src/main/java/dev/kellyson/fighthub/controller/AlunoController.java
+++ src/main/java/dev/kellyson/fighthub/controller/AlunoController.java
@@ -1,11 +1,14 @@
 package dev.kellyson.fighthub.controller;
 
 import dev.kellyson.fighthub.dto.AlunoRequest;
+import dev.kellyson.fighthub.dto.PerfilTreinoRequest;
 import dev.kellyson.fighthub.service.AlunoService;
+import dev.kellyson.fighthub.service.PerfilTreinoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,10 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class AlunoController {
 
     private final AlunoService alunoService;
+    private final PerfilTreinoService perfilTreinoService;
 
     @PostMapping
     public ResponseEntity<Void> cadastrarAluno(@RequestBody @Valid AlunoRequest alunoRequest) {
         alunoService.cadastrarAluno(alunoRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/{alunoId}/perfil-treino")
+    public ResponseEntity<Void> definirPerfilDeTreino(@PathVariable Long alunoId,
+                                                      @RequestBody @Valid PerfilTreinoRequest perfilTreinoRequest) {
+        perfilTreinoService.definirPerfilDeTreino(alunoId, perfilTreinoRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
