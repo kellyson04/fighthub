cdb433c1ff64d83405ee10ea05537fe4fac69f1b
Kell <gilsonvigilante@outlook.com>
2026-07-25 00:51:42 -0300

    feat: cria enum de dia da semana

---
+++ src/main/java/dev/kellyson/fighthub/enums/DiaDaSemana.java
@@ -0,0 +1,11 @@
+package dev.kellyson.fighthub.enums;
+
+public enum DiaDaSemana {
+    SEGUNDA_FEIRA,
+    TERCA_FEIRA,
+    QUARTA_FEIRA,
+    QUINTA_FEIRA,
+    SEXTA_FEIRA,
+    SABADO,
+    DOMINGO
+}
