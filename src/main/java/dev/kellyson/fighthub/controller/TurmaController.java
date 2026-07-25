62aefdc275e8834f0e235aba633ed998b820e907
Kell <gilsonvigilante@outlook.com>
2026-07-25 00:52:24 -0300

    feat: adiciona endpoint para matricular aluno na turma

--- src/main/java/dev/kellyson/fighthub/controller/TurmaController.java
+++ src/main/java/dev/kellyson/fighthub/controller/TurmaController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,13 @@ public class TurmaController {
     @PostMapping
     public ResponseEntity<Void> cadastrarTurma(@RequestBody @Valid TurmaRequest turmaRequest) {
         turmaService.cadastrarTurma(turmaRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/{turmaId}/alunos/{alunoId}")
+    public ResponseEntity<Void> matricularAlunoNaTurma(@PathVariable Long turmaId,
+                                                       @PathVariable Long alunoId) {
+        turmaService.matricularAlunoNaTurma(turmaId, alunoId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
