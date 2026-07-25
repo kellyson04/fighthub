be5edbd13c6f457855c13e44b371c5601c1062ad
Kell <gilsonvigilante@outlook.com>
2026-07-25 20:40:11 -0300

    feat: adiciona endpoint para consultar assiduidade

--- src/main/java/dev/kellyson/fighthub/controller/PresencaController.java
+++ src/main/java/dev/kellyson/fighthub/controller/PresencaController.java
@@ -1,9 +1,11 @@
 package dev.kellyson.fighthub.controller;
 
+import dev.kellyson.fighthub.dto.AssiduidadeResponse;
 import dev.kellyson.fighthub.service.PresencaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +22,11 @@ public class PresencaController {
     public ResponseEntity<Void> registrarPresenca(@PathVariable Long matriculaId) {
         presencaService.confirmarPresenca(matriculaId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("{matriculaId}/assiduidade")
+    public ResponseEntity<AssiduidadeResponse> consultarAssiduidade(@PathVariable Long matriculaId) {
+        AssiduidadeResponse assiduidade = presencaService.consultarAssiduidade(matriculaId);
+        return ResponseEntity.ok(assiduidade);
     }
 }
