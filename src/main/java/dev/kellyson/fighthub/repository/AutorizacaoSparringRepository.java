860511c746a1c96f8e170531d384d88dcfe50449
Kell <gilsonvigilante@outlook.com>
2026-07-26 22:10:49 -0300

    feat: cria repository para registrar autorizações de sparring

---
+++ src/main/java/dev/kellyson/fighthub/repository/AutorizacaoSparringRepository.java
@@ -0,0 +1,7 @@
+package dev.kellyson.fighthub.repository;
+
+import dev.kellyson.fighthub.entity.AutorizacaoSparring;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorizacaoSparringRepository extends JpaRepository<AutorizacaoSparring, Long> {
+}
